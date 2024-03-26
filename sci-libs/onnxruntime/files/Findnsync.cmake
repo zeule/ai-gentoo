f03d0561ac36d7604fde6eaa0673c89cab2bd2e6
Eugne Shalygin <e.shalygin@abberior-instruments.com>
2024-03-26 12:43:02 +0100

    add onnxruntime from the cg overlay
    and make openmpi dep optional

--- sci-libs/onnxruntime/files/Findnsync.cmake
+++ sci-libs/onnxruntime/files/Findnsync.cmake
@@ -1,5 +1,9 @@
 find_path(NSYNC_INCLUDE_DIR nsync.h)
 find_library(NSYNC_LIBRARY nsync)
+find_library(NSYNC_CPP_LIBRARY nsync_cpp)
+
+message(STATUS "DGSDGDSGDSGDSGSDGDFGDSGDGF")
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(${CMAKE_FIND_PACKAGE_NAME}
 	FOUND_VAR ${CMAKE_FIND_PACKAGE_NAME}_FOUND
@@ -9,10 +13,18 @@ find_package_handle_standard_args(${CMAKE_FIND_PACKAGE_NAME}
 )
 
 if (${CMAKE_FIND_PACKAGE_NAME}_FOUND)
-	add_library(nsync::nsync_cpp SHARED IMPORTED GLOBAL)
-	set_target_properties(nsync::nsync_cpp
+	add_library(nsync SHARED IMPORTED GLOBAL)
+	set_target_properties(nsync
 		PROPERTIES
 			INTERFACE_INCLUDE_DIRECTORIES "${NSYNC_INCLUDE_DIR}"
-			INTERFACE_LINK_LIBRARIES ${NSYNC_INCLUDE_DIR}
-   )
+			INTERFACE_LINK_LIBRARIES ${NSYNC_LIBRARY}
+	)
+	if (NSYNC_CPP_LIBRARY)
+		add_library(nsync_cpp SHARED IMPORTED GLOBAL)
+	        set_target_properties(nsync_cpp
+                PROPERTIES
+                        INTERFACE_INCLUDE_DIRECTORIES "${NSYNC_INCLUDE_DIR}"
+                        INTERFACE_LINK_LIBRARIES ${NSYNC_CPP_LIBRARY}
+        	)
+	endif()
 endif()
